cdd478d079c12a1bdea2a692cd4af9c436ea6cc6
Tello <fredy9418@hotmail.com>
2021-09-06 17:27:04 -0500

    respondive circular progres bar

--- src/Molecules/CircularProgressBar.jsx
+++ src/Molecules/CircularProgressBar.jsx
@@ -8,57 +8,33 @@ import 'react-circular-progressbar/dist/styles.css';
 
 const CircularProgressBar = () => {
     const percentage = 40;
-    return (
-        <CircularProgressbar
-            percentage={percentage}
-            text={`${percentage}%`}
-            value={40}
-            strokeWidth={8}
-            // background={true}
-            styles={{
-                path: {
-                    // Path color
-                    stroke: '#FED300',
-                    // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
-                    strokeLinecap: 'round',
-                    // Customize transition animation
-                    transition: 'stroke-dashoffset 0.5s ease 0s',
-                },
-                trail: {
-                    // Trail color                    
-                    stroke:'#FF7A4F'
-                },
-                // Customize the text
-                text: {
-                    // Text color
-                    fill:'white',  
-                    fontFamily:'gilroy-bold',                  
-                    // Text size
-                    fontSize: '2.8vh',
-                    
-                },
-                // Customize background - only used when the `background` prop is true
-                background: {
-                    // fill: '#3e98c7',
-                    // fill:'##FF7A4F'
-                },
-
-            }}
-        />
-        // <div className="circularprogressbar-div">
-        //     <div className="circular">
-        //         <div className="inner"></div>
-        //         <div className="number">40%</div>
-        //         <div className="circle">
-        //             <div className="barleft">
-        //                 <div className="progress"></div>
-        //             </div>
-        //             <div className="barright">
-        //                 <div className="progress"></div>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </div>
+    return (        
+            <CircularProgressbar
+                percentage={percentage}
+                text={`${percentage}%`}
+                value={40}
+                strokeWidth={8}               
+                styles={{                
+                    path: {
+                        // Path color
+                        stroke: '#FED300',
+                        // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+                        strokeLinecap: 'round',
+                        // Customize transition animation
+                        transition: 'stroke-dashoffset 0.5s ease 0s',
+                    },
+                    trail: {
+                        // Trail color                    
+                        stroke: '#FF7A4F'
+                    },
+                    // Customize the text
+                    text: {                    
+                        fill: 'white',
+                        fontFamily: 'gilroy-bold',                      
+                        fontSize: '2.8vh',
+                    },                             
+                }}
+            />        
     );
 };
 
--- src/Organisms/ProgressOverview.jsx
+++ src/Organisms/ProgressOverview.jsx
@@ -2,19 +2,17 @@ import React from "react";
 
 import MyTitle from "../Atoms/MyTitle";
 import CircularProgressBar from "../Molecules/CircularProgressBar";
-// import CircularTest from "../Molecules/CircularTest";
 import ProgressHours from "../Molecules/ProgressHours";
 
 import "../Styles/Organisms/ProgressOverview.css";
 
-const ProgressOverview=()=>{
-    return(
-    <div className="progressoverview-div">
-        <MyTitle isProgress={true} text="Progress overview"/>
-        <CircularProgressBar/>
-        {/* <CircularTest/> */}
-        <ProgressHours/>
-    </div>
+const ProgressOverview = () => {
+    return (
+        <div className="progressoverview-div">
+            <MyTitle isProgress={true} text="Progress overview" />
+            <div className="circularprogress-div"><CircularProgressBar /></div>
+            <ProgressHours />
+        </div>
     );
 };
 
